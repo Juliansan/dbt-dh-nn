579cee60ec2fd73a0623bab4a907c278397420d2
julian <julsanm@gmail.com>
2022-05-14 13:27:40 +0200

    test profiles with entrypoint

---
+++ dbt_project/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+export DBT_PROFILES_DIR="${GITHUB_WORKSPACE}/dbt_project"
+export DBT_PROJECT_FOLDER="$GITHUB_WORKSPACE/dbt_project"
+
+echo "dbt project folder set as \"${DBT_PROJECT_FOLDER}\""
+cd ${DBT_PROJECT_FOLDER}
+
+echo connection settings to snowflake
+
+sed -i "s/_account_/${SNOWFLAKE_ACCOUNT}/g" ./profiles.yml
+sed -i "s/_user_/${SNOWFLAKE_USER}/g" ./profiles.yml
+sed -i "s/_password_/${SNOWFLAKE_PASSWORD}/g" ./profiles.yml
+sed -i "s/_role_/${SNOWFLAKE_ROLE}/g" ./profiles.yml
+sed -i "s/_db_/${SNOWFLAKE_DB}/g" ./profiles.yml
+sed -i "s/_wh_/${SNOWFLAKE_WH}/g" ./profiles.yml
+sed -i "s/_schema_/${SNOWFLAKE_SCHEMA}/g" ./profiles.yml
